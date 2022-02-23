1b028bb17fe38436e44cb10d06a5dd37173c60c8
Filip Asprovski <filip.asprovski@sourcico.com>
2022-02-23 09:59:32 +0100

    ADD route for adding new player

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import routes from './routes/soccerRoutes';
 
 const app = express();
 const PORT = 3000;
@@ -16,6 +17,8 @@ mongoose.connect('mongodb://localhost/soccerDb', {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+routes(app);
 
 app.get('/', (req, res) => {
     res.send(`Our Soccer application is running on port: ${PORT}`);
